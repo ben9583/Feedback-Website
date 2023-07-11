5ed176d259cf5847fb6a1bda1e8cb9c2f9ff1183
Ben Plate <bplate9583@gmail.com>
2023-07-10 23:55:25 -0700

    implement feedback form

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  })],
   experimental: {
-    assets: true,
+    assets: true
   },
-  output: "server"
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
