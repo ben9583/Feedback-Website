877da4478234c57b594f52819d0d0607f4554d2f
Ben Plate <bplate9583@gmail.com>
2023-07-12 17:25:37 -0700

    fix db client

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,16 @@
 import pg from 'pg'
 import Config from './config'
 
-const Client = new pg.Client({
-  database: Config.postgres.database,
-  user: Config.postgres.user,
-  password: Config.postgres.password,
-  host: Config.postgres.host,
-  port: Config.postgres.port,
-})
+class Client extends pg.Client {
+  constructor() {
+    super({
+      database: Config.postgres.database,
+      user: Config.postgres.user,
+      password: Config.postgres.password,
+      host: Config.postgres.host,
+      port: Config.postgres.port,
+    })
+  }
+}
 
 export default Client
