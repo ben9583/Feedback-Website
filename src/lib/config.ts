d75ee08dfcd2bb3d2e9ea3f7280b53f98ffbec60
Ben Plate <bplate9583@gmail.com>
2023-07-12 21:22:04 -0700

    Implement authentication

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,23 +1,6 @@
 import fs from 'fs'
 import yaml from 'js-yaml'
 
-/*
-website:
-  title: Feedback Website
-  description: Use this website to provide feedback.
-
-postgres:
-  database: feedback-db
-  user: feedback-user
-  password: feedback-password
-  host: localhost
-  port: 5432
-
-admin:
-  name: admin
-  password: CHANGE_ME_OR_ELSE
-*/
-
 export type ConfigType = {
   website: {
     title: string
@@ -31,8 +14,8 @@ export type ConfigType = {
     port: number
   }
   admin: {
-    name: string
-    password: string
+    username: string
+    passkey: string
   }
 }
 
