d43ab2d2d31eb685f42cea4ed4cb5ca780f13d1f
Ben Plate <bplate9583@gmail.com>
2023-07-13 13:58:11 -0700

    implement view feedback

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -13,4 +13,15 @@ class Client extends pg.Client {
   }
 }
 
+export interface DatabaseEntry {
+  id: number
+  for_user: string
+  from_user: string
+  code: string
+  created_on: string
+  submitted: boolean
+  response: string
+  submitted_on: string
+}
+
 export default Client
